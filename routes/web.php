c1306cdbb99138b5f54c5b8a8ab477a5a0341166
Leena <vegdaleena44@gmail.com>
2026-06-27 12:38:22 +0530

    Revert "Add practice blade file and assets"
    This reverts commit 39ca0071d94a56f6f92915b281356e54e5c6d02b.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [LandingController::class, 'index'])->name('landing');
+Route::get('/', [LandingController::class, 'index'])->name('landing');
 Route::post('/contact', [LandingController::class, 'store'])->name('landing.contact');
-Route::view('/', 'practice')->name('practice');
